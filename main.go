04bd5a7418285e8399ae6314b7f4ede94db7620e
Erin Keenan <erinok@gmail.com>
2018-08-02 09:58:45 -0700

    output tsv flashcard file

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"runtime"
@@ -16,13 +17,22 @@ import (
 var (
 	srtFile  = flag.String("srt", "", "`SRT` subtitles of movie's spoken audio")
 	movFile  = flag.String("mov", "", "extract mp3 clips from `MOVIE`")
+	xsrtFile = flag.String("xsrt", "", "`SRT` translated subtitles")
 	xbefore  = flag.Duration("xbefore", 500*time.Millisecond, "include `DUR` time before each audio clip")
 	xafter   = flag.Duration("xafter", 2000*time.Millisecond, "include `DUR` time after each audio clip")
 	numCores = flag.Int("numCore", 2*runtime.NumCPU(), "use up to `CORES` threads while converting audio")
 )
 
+func clipFname(idx int, item *astisub.Item) string {
+	return fmt.Sprint(*movFile, ".", idx+1, ".mp3")
+}
+
 func extractAudioClip(idx int, item *astisub.Item) {
-	clipFile := fmt.Sprint(*movFile, ".", idx, ".mp3")
+	fname := clipFname(idx, item)
+	if stat, err := os.Stat(fname); err == nil && stat.Size() > 0 {
+		// clip already exists; do nothing
+		return
+	}
 	ss := (item.StartAt - *xbefore).Seconds()
 	t := (item.EndAt + *xafter).Seconds() - ss
 	cmd := exec.Command("ffmpeg",
@@ -30,7 +40,7 @@ func extractAudioClip(idx int, item *astisub.Item) {
 		"-i", *movFile,
 		"-ss", fmt.Sprintf("%.03f", ss),
 		"-t", fmt.Sprintf("%.03f", t),
-		clipFile,
+		clipFname(idx, item),
 	)
 	fmt.Println(">", strings.Join(cmd.Args, " "))
 	buf, err := cmd.CombinedOutput()
@@ -39,25 +49,73 @@ func extractAudioClip(idx int, item *astisub.Item) {
 	}
 }
 
+func fmtSub(sub *astisub.Item) string {
+	s := sub.String()
+	s = strings.Replace(s, "\n", "<br/>", -1)
+	s = strings.Replace(s, "\t", " ", -1)
+	return s
+}
+
+func fmtSubs(subs []*astisub.Item) string {
+	var s string
+	for i, sub := range subs {
+		if i > 0 {
+			s += "<br/>"
+		}
+		s += fmtSub(sub)
+	}
+	return s
+}
+
+// return the items in subs that overlap sub
+func overlappingSubs(sub *astisub.Item, subs []*astisub.Item) []*astisub.Item {
+	i := 0
+	for i < len(subs) && !(sub.StartAt <= subs[i].EndAt) {
+		i++
+	}
+	j := i
+	for j < len(subs) && !(sub.EndAt < subs[j].StartAt) {
+		j++
+	}
+	return subs[i:j]
+}
+
 // outputs:
 //
 // orig text
 // trans text
 // audio
+func writeFlashcards(f io.Writer, subs, xsubs *astisub.Subtitles) {
+	for i, item := range subs.Items {
+		xitems := overlappingSubs(item, xsubs.Items)
+		fmt.Fprintln(f, fmtSub(item), "\t", fmtSubs(xitems), "\t", clipFname(i, item))
+	}
+}
 
 func main() {
 	flag.Parse()
-	srt, err := astisub.OpenFile(*srtFile)
+	if *movFile == "" || *srtFile == "" {
+		fatal("must pass -mov, -srt, and -xrt")
+	}
+	subs, err := astisub.OpenFile(*srtFile)
 	if err != nil {
 		fatal(err)
 	}
-	if *movFile != "" {
-		parallelDo(len(srt.Items), *numCores, func(i int) { 
-			extractAudioClip(i+1, srt.Items[i]) 
-		})
+	parallelDo(len(subs.Items), *numCores, func(i int) {
+		extractAudioClip(i, subs.Items[i])
+	})
+	xsubs, err := astisub.OpenFile(*xsrtFile)
+	if err != nil {
+		fatal(err)
 	}
+	f, err := os.Create(*movFile + ".cards.tsv")
+	if err != nil {
+		fatal(err)
+	}
+	defer f.Close()
+	writeFlashcards(f, subs, xsubs)
 	if false {
-		for _, item := range srt.Items {
+		for _, item := range subs.Items {
 			fmt.Println(item.String())
 		}
 	}
